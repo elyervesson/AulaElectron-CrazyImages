779fea21e57e001f550787afcd73518588299219
Elyervesson de Souza <elyervesson@gmail.com>
2017-08-23 16:19:38 -0300

    Finalizada a primeira aula

---
+++ README.md
@@ -0,0 +1,8 @@
+# AulaElectron-CrazyImages
+
+**Codigo das aulas de criação de um app desktop com uso do Electron + AngularJS**
+
+Este curso tem um total de 10 aulas
+
+## Links
+- Aula 01 - https://youtu.be/qdCFQOWALRw
---
+++ main.js
@@ -0,0 +1,19 @@
+const {BrowserWindow, app} = require('electron');
+const path = require('path');
+
+let win;
+let boot = () => {
+  win = new BrowserWindow({
+    width: 700,
+    height: 500,
+    frame: false
+  });
+
+  // Carrega a aplicação no template
+  win.loadURL('file://' + __dirname + '/index.html');
+
+  // Abre automaticamente o developer tools quando a janela for criada
+  win.webContents.openDevTools();
+};
+
+app.on('ready', boot);
